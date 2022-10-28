6cf46837623aa39b9e12754fd2df2603889d975b
midhyun <kfree912@gmail.com>
2022-10-28 17:44:15 +0900

    fisrt deploy_

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_ROOT = BASE_DIR / "/media/"
 MEDIA_URL = '/images/'
 
 # Default primary key field type
